acc00552e7b20d3871226c7160c927999223afcd
Haley Warson <haleywarson@gmail.com>
2021-01-30 20:24:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,11 +4,11 @@ def mario
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
 def link
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "It's Dangerous To Go Alone! Take This."
   puts status
 end
