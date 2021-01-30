f36dfc77ea15abd3bb5ff6229645b8ed5f029bd8
Haley Warson <haleywarson@gmail.com>
2021-01-30 20:37:19 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,18 +1,18 @@
-mario_phrase = "It's-a me, Mario!"
 def mario
+  mario_phrase = "It's-a me, Mario!"
   puts mario_phrase
 end
 
-# toadstool_phrase = "Thank You Mario! But Our Princess Is In Another Castle!"
-# def toadstool
-#   puts toadstool_phrase
-# end
+toadstool_phrase = "Thank You Mario! But Our Princess Is In Another Castle!"
+def toadstool
+  puts toadstool_phrase
+end
 
-# link_phrase = "It's Dangerous To Go Alone! Take This."
-# def link
-#   puts link_phrase
-# end
+link_phrase = "It's Dangerous To Go Alone! Take This."
+def link
+  puts link_phrase
+end
 
-# def all_phrases
-#   puts mario_phrase, toadstool_phrase, link_phrase
-# end
+def all_phrases
+  puts mario_phrase, toadstool_phrase, link_phrase
+end
