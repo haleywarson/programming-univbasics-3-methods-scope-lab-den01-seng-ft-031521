8c2b681b0d74c900b74b253198085bd86dcbf66d
Haley Warson <haleywarson@gmail.com>
2021-01-30 20:18:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,2 @@
-def mario
-  phrase = 'It's-a me, Mario!
-  puts phrase
-end
-
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-def toadstool
-  puts status
-end
+define link(phrase)
+  phrase = 
